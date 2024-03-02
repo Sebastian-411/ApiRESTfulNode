25678533caea352c69d44a3645d9efb31fb9fc81
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-03-02 15:12:51 -0500

    First steps

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) =>{
+    res.status(200).json({
+        message: 'Its working'
+    });
+
+});
+
+module.exports = app;
